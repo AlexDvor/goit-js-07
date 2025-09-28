ee9ce4d9126ad93a62b9ced5fecabf9abb0f8cd9
AlexDvor <alexmatvichuk@gmail.com>
2025-09-28 10:17:20 +0200

    Add Task

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -1,1 +1,14 @@
-console.log('fff');
+const refs = {
+  categoriesList: document.querySelector('#categories'),
+  items: document.querySelectorAll('#categories .item'),
+};
+
+console.log(`Number of categories: ${refs.categoriesList.children.length}`);
+
+refs.items.forEach(item => {
+  const title = item.querySelector('h2').textContent.trim();
+  const elementsCount = item.querySelector('ul').children.length;
+
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
+});
